7a916644d7b19c93eeaef63fc938f1ccba23288e
sunguk <qkrtjddnr147@gmail.com>
2025-08-27 16:54:25 +0900

    코드 순서 수정

--- Assets/0. Develop/02. Scripts/1) System/Managers/GameManager.cs
+++ Assets/0. Develop/02. Scripts/1) System/Managers/GameManager.cs
@@ -125,8 +125,8 @@ public class GameManager : MonoBehaviour, IPullManager
 
         // 자동차 세팅
         PlayerCarSet(isMain);
-        player.GetCarData(carDatas[CurCarIndex]);
         _audioMgr.BgmSoundPlay(isMain);
+        player.GetCarData(carDatas[CurCarIndex]);
 
         if (isMain)
         {
